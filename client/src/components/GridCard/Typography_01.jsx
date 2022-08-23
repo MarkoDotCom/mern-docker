f3fe25c627cf6f714efee71ecdd702cec9986ee8
MSGB <51678506+MarkoDotCom@users.noreply.github.com>
2022-08-23 10:35:47 -0400

    feat mejora de aspecto estable gridcard (#8)

---
+++ client/src/components/GridCard/Typography_01.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Typography from "@mui/material/Typography";
+
+const Typography_01 = () => {
+  return (
+    <Typography
+      xs={6}
+      variant="h6"
+      sx={{
+        fontWeight: "bold",
+      }}
+    >
+      Typography_01
+    </Typography>
+  );
+};
+
+export default Typography_01;
